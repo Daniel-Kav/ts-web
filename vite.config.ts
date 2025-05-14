ecf63f4a282e13037cff96c9edf7383acd5900bd
daniel-kav <dankavatha01@gmail.com>
2025-05-14 10:35:44 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   root: 'src',
+  base: '/ts-web/', // Replace with your repository name
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true
   }
-};
+});
